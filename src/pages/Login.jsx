b4e111cdcca5a423e5a4d03a00f1ed5ab110419c
seongsinhye <sinhye2428@naver.com>
2024-11-27 15:17:41 +0900

    로그인까지 구현

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { useState } from 'react';
 import UserInput from '../components/common/UserInput';
 import UserButton from '../components/common/UserButton';
+import { useNavigate } from 'react-router-dom';
 import { IoPersonCircleOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
+import api from '../util/fetchFunc';
 
 export default function Login() {
   const [userInfo, setUserInfo] = useState({
-    email: '',
-    password: '',
+    id: '',
+    pw: '',
   });
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -16,6 +18,30 @@ export default function Login() {
       ...userInfo,
       [name]: value,
     }));
+  };
+  const navigate = useNavigate();
+
+  const loginProgcess = () => {
+    fetch(api.BASE_URL + '/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        id: userInfo.id,
+        pw: userInfo.pw,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === 200) {
+          alert('로그인 되었습니다.');
+          localStorage.setItem('token', data.token);
+          navigate('/');
+        } else {
+          alert('가입되지 않은 정보입니다.');
+        }
+      });
   };
 
   return (
@@ -25,20 +51,20 @@ export default function Login() {
           <IoPersonCircleOutline className='personIcon' />
           <UserInput
             type='text'
-            placeholder='이메일'
-            value={userInfo.email}
-            name='email'
+            placeholder='아이디'
+            value={userInfo.id}
+            name='id'
           />
           <UserInput
             type='password'
             placeholder='비밀번호'
-            value={userInfo.password}
-            name='password'
+            value={userInfo.pw}
+            name='pw'
           />
           <UserButton
             text='로그인'
             // disabled={!isInvaild}
-            // onClick={loginProgcess}
+            onClick={loginProgcess}
           />
           <Link to='/join' className='no-underline text-neutral-700 block'>
             회원가입
---
+++ src/util/fetchFunc.js
@@ -0,0 +1,4 @@
+export const BASE_URL = "http://www.danchu.store:8080/danchu_back/api";
+
+
+export default  BASE_URL ;
